c639916986ffcc15b5427c4f116b7c1a9ee8d33e
Sabfes <bitaev.db@gmail.com>
2023-01-13 09:11:05 +0300

    react app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-const light = document.querySelector('.header__image')
-const appWrap = document.querySelector('.container')
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-light.addEventListener('click', () => {
-  if (appWrap.style.background === 'black') {
-    appWrap.style.background = 'white'
-    light.style.opacity = '1'
-  } else {
-    appWrap.style.background = 'black'
-    light.style.opacity = '.2'
-  }
-})
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
