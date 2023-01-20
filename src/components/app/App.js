799c075d1335e4761badd26b29ce61fd6b9b8d12
Sabfes <bitaev.db@gmail.com>
2023-01-20 14:33:30 +0300

    test-task

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,29 +1,96 @@
 import './App.css';
-import StartPage from "../../pages/startPage/StartPage";
-import {AppContext} from "../../context/appContext";
-import {useEffect, useState} from "react";
+import {useState} from "react";
+
 
 function App() {
-  const [state, setState] = useState({
-    theme: 'light'
-  })
+  const [state, setState] = useState([
+    {
+      id: 'a12da.smd,msdasdasd12',
+      name: 'Folders',
+    },
+    {
+      id: '12lamsdlml2',
+      name: 'Projects',
+    },
+    {
+      id: 'asdasdasd12',
+      name: 'About',
+      children: [
+        {
+          id: 'asda123123sdasd12',
+          name: 'About__2',
+          children:  [
+            {
+              id: 'asda121231233123sdasd12',
+              name: 'About__3',
+              children:  []
+            },
+            {
+              id: 'asda1231asdasd23sdasd12',
+              name: 'About__3.2',
+              children:  []
+            },
+          ]
+        },
+        {
+          id: 'asda123123sd123asdasd12',
+          name: 'About__2.2',
+          children:  []
+        }
+      ]
+    }
+  ])
+  const [displayChildren, setDisplayChildren] = useState({});
 
-  const changeState = (title, args) => {
-    setState({
-      ...state,
-      [title]: args
-    })
+  const renderChild = (children, level) => {
+    const test = children.map(el => {
+      return <div key={el.id}>
+        <div
+          onClick={ () => {
+            setDisplayChildren({
+              ...displayChildren,
+              [el.name]: !displayChildren[el.name],
+            });
+          }}
+          style={{marginLeft: level * 40 + 'px'}}
+          className={`test ${displayChildren[el.name] && 'active'}`}>
+          {el.name}
+        </div>
+        {
+          el.children && displayChildren[el.name] && <div className={'test__child'}>
+            {renderChild(el.children, level + 1)}
+          </div>
+        }
+      </div>
+      })
+    return <div>
+      {test}
+    </div>
   }
 
-  useEffect(() => {
-    console.log(state)
-  }, [state])
-
   return (
-    <div className={`App app_theme_${state.theme}`}>
-      <AppContext.Provider value={{...state, changeState}}>
-        <StartPage/>
-      </AppContext.Provider>
+    <div className={'App'}>
+      {
+        state.map((el) => {
+          return <div key={el.id}>
+            <div
+              onClick={() => {
+                setDisplayChildren({
+                  ...displayChildren,
+                  [el.name]: !displayChildren[el.name],
+                });
+              }}              className={`test ${displayChildren[el.name] && 'active'}`}
+              >
+              {el.name}
+            </div>
+            {
+              el.children && displayChildren[el.name] && <div className={'test__child'}>
+                {renderChild(el.children, 1)}
+              </div>
+            }
+          </div>
+        })
+      }
     </div>
   );
 }
--- src/pages/startPage/__footer/Footer.js
+++ src/pages/startPage/__footer/Footer.js
@@ -7,7 +7,6 @@ const Footer = () => {
 
   return <div className={`footer theme_${theme}`}>
     <nav className={'footer__links'}>
-
     </nav>
   </div>
 }
